d619d9fb92586f12f98613e4f01992394bafed23
Markus Chiarot <cmarkus@hotmail.ch>
2021-01-03 02:35:48 +0100

    adjust control

--- src/gameobjects/fish.ts
+++ src/gameobjects/fish.ts
@@ -4,6 +4,8 @@ import { DialogType, DialogManager } from '../util/dialog-manager';
 
 export class Fish extends Phaser.Physics.Arcade.Sprite {
   public body: Phaser.Physics.Arcade.Body;
+  public spaceKey: Phaser.Input.Keyboard.Key;
+  public upKey: Phaser.Input.Keyboard.Key;
   private isDead = false;
   private speed = 250;
 
@@ -17,10 +19,16 @@ export class Fish extends Phaser.Physics.Arcade.Sprite {
     this.depth = 1;
     this.setMaxVelocity(this.speed, 500);
     this.setX(x + this.displayWidth);
+
+    // add controls
+    this.spaceKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.upKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    const onDown = () => this.setVelocityY(-(GRAVITY.y / 2));
+    this.spaceKey.on('down', () => onDown());
+    this.upKey.on('down', () => onDown());
   }
 
   protected preUpdate(): void {
-    const { space, up } = this.scene.input.keyboard.createCursorKeys();
     this.setVelocityX(this.speed);
 
     if (this.isDead) {
@@ -31,10 +39,6 @@ export class Fish extends Phaser.Physics.Arcade.Sprite {
       } else if (this.y - this.displayWidth / 2 === 0) {
         this.scene.scene.pause();
       }
-    } else {
-      if (space.isDown || up.isDown) {
-        this.setVelocityY(this.body.velocity.y - 40);
-      }
     }
   }
 
@@ -42,8 +46,15 @@ export class Fish extends Phaser.Physics.Arcade.Sprite {
     if (this.isDead) return;
     this.isDead = true;
 
-    DialogManager.openDialog(DialogType.GAMEOVER);
+    // remove all keyboard inputs
+    this.spaceKey.removeAllListeners();
+    this.upKey.removeAllListeners();
+
+    // make swim up
     this.setVelocity(0, 0);
     this.setGravityY(0 - GRAVITY.y - 100);
+
+    // open dialog
+    DialogManager.openDialog(DialogType.GAMEOVER);
   }
 }
